c8839733e0add49e4fbba1218f4fa27e33f04e1c
Said Abdulsalam <saidabdulsalam5@gmail.com>
2021-10-23 09:21:17 +0100

    update: controls

--- resources/views/viewpdf.blade.php
+++ resources/views/viewpdf.blade.php
@@ -1,3 +1,18 @@
+
+<?php
+
+if (empty($_COOKIE['mycookiexyz123'])) {
+    header('Cache-Control: no-store, no-cache, must-revalidate, post-check=0, pre-check=0, max-age=0');
+}else{
+    if($_COOKIE[''] ===  date('Y-m-d')){
+
+    }else{
+        $_COOKIE['mycookiexyz123'] = date('Y-m-d');
+        header('Cache-Control: no-store, no-cache, must-revalidate, post-check=0, pre-check=0, max-age=0');
+    }
+}
+?>
+
 @extends('layouts.app')
 
 @section('content')
