1a3c1bf9a701e2f488682cc56c830cd33a7ec24e
Said Abdulsalam <saidabdulsalam5@gmail.com>
2021-10-23 09:43:27 +0100

    update: controls

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,7 +12,7 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js?v='.filemtime('js/your.js')') }}" defer></script>
+    <script src="{{ asset('js/app.js?v=2238337373192') }}" defer></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
